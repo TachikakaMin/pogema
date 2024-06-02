2d8e01e5a480ee2d39ec30d5a640c2db2457f0e9
tviskaron <tviskaron@gmail.com>
2024-06-02 18:01:00 +0300

    Refactored animations

--- pogema/svg_animation/animation_wrapper.py
+++ pogema/svg_animation/animation_wrapper.py
@@ -1,10 +1,9 @@
 import os
-import typing
 from itertools import cycle
 from gymnasium import logger, Wrapper
 
-from pogema import GridConfig, pogema_v0, BatchAStarAgent
-from pogema.animation_drawer import AnimationConfig, AnimationSettings, GridHolder, AnimationDrawer
+from pogema import GridConfig
+from pogema.svg_animation.animation_drawer import AnimationConfig, SvgSettings, GridHolder, AnimationDrawer
 from pogema.wrappers.persistence import PersistentWrapper, AgentState
 
 
@@ -21,7 +20,7 @@ class AnimationMonitor(Wrapper):
 
         self.history = env.get_history()
 
-        self.svg_settings: AnimationSettings = AnimationSettings()
+        self.svg_settings: SvgSettings = SvgSettings()
         self.animation_config: AnimationConfig = animation_config
 
         self._episode_idx = 0
@@ -88,63 +87,85 @@ class AnimationMonitor(Wrapper):
 
         return obs
 
-    def save_animation(self, name='render.svg', animation_config: typing.Optional[AnimationConfig] = AnimationConfig()):
+    def save_animation(self, name='render.svg', animation_config: AnimationConfig = AnimationConfig()):
         """
         Saves the animation.
         :param name: name of the file
         :param animation_config: animation configuration
         :return: None
         """
-        # animation = self.create_animation(animation_config)
         wr = self._working_radius
         obstacles = self.env.get_obstacles(ignore_borders=False)[wr:-wr, wr:-wr]
         history: list[list[AgentState]] = self.env.decompress_history(self.history)
 
-        episode_length = len(history[0])
-        svg_settings = AnimationSettings()
+        svg_settings = SvgSettings()
         colors_cycle = cycle(svg_settings.colors)
         agents_colors = {index: next(colors_cycle) for index in range(self.grid_config.num_agents)}
+
+        for agent_idx in range(self.grid_config.num_agents):
+            history[agent_idx].append(history[agent_idx][-1])
+
+        episode_length = len(history[0])
+        # Change episode length for egocentric environment
+        if animation_config.egocentric_idx is not None and self.grid_config.on_target == 'finish':
+            episode_length = history[animation_config.egocentric_idx][-1].step + 1
+            for agent_idx in range(self.grid_config.num_agents):
+                history[agent_idx] = history[agent_idx][:episode_length]
+
         grid_holder = GridHolder(
             width=len(obstacles), height=len(obstacles[0]),
             obstacles=obstacles,
             episode_length=episode_length,
             history=history,
             obs_radius=self.grid_config.obs_radius,
-            colors=agents_colors,
             on_target=self.grid_config.on_target,
+            colors=agents_colors,
+            config=animation_config,
+            svg_settings=svg_settings
         )
 
-        animation = AnimationDrawer().create_animation(grid_holder, animation_config=animation_config)
+        animation = AnimationDrawer().create_animation(grid_holder)
         with open(name, "w") as f:
             f.write(animation.render())
 
 
 def main():
-    grid = """
-    ...#.
-    .#...
-    .....
-    .....
-    ##.#.
-    ##.#.
-    """
-    grid_config = GridConfig(size=32, num_agents=2, obs_radius=2, seed=11, on_target='finish', max_episode_steps=16,
-                             density=0.1, map=grid, observation_type="POMAPF")
-    env = pogema_v0(grid_config=grid_config)
-    env = AnimationMonitor(env)
+    from pogema import GridConfig, pogema_v0, AnimationMonitor, BatchAStarAgent, AnimationConfig
 
-    obs, _ = env.reset()
-    truncated = terminated = [False]
+    for egocentric_idx in [0, 1]:
+        for on_target in ['nothing', 'restart', 'finish']:
+            grid = """
+            ....#..
+            ..#....
+            .......
+            .......
+            #.#.#..
+            #.#.#..
+            """
+            grid_config = GridConfig(size=32, num_agents=2, obs_radius=2, seed=8, on_target=on_target,
+                                     max_episode_steps=16,
+                                     density=0.1, map=grid, observation_type="POMAPF")
+            env = pogema_v0(grid_config=grid_config)
+            env = AnimationMonitor(env, AnimationConfig(save_every_idx_episode=None))
 
-    agent = BatchAStarAgent()
-    while not all(terminated) and not all(truncated):
-        obs, _, terminated, truncated, _ = env.step(agent.act(obs))
+            obs, _ = env.reset()
+            truncated = terminated = [False]
 
-    env.save_animation('out-static.svg', AnimationConfig(static=True, save_every_idx_episode=None))
-    env.save_animation('out-static-ego.svg', AnimationConfig(egocentric_idx=0, static=True))
-    env.save_animation('out-static-no-agents.svg', AnimationConfig(show_agents=False, static=True))
-    env.save_animation("out.svg")
-    env.save_animation("out-ego.svg", AnimationConfig(egocentric_idx=0))
+            agent = BatchAStarAgent()
+            while not all(terminated) and not all(truncated):
+                obs, _, terminated, truncated, _ = env.step(agent.act(obs))
+
+            anim_folder = 'renders'
+            if not os.path.exists(anim_folder):
+                os.makedirs(anim_folder)
+
+            env.save_animation(f'{anim_folder}/anim-{on_target}.svg')
+            env.save_animation(f'{anim_folder}/anim-{on_target}-ego-{egocentric_idx}.svg',
+                               AnimationConfig(egocentric_idx=egocentric_idx))
+            env.save_animation(f'{anim_folder}/anim-static.svg', AnimationConfig(static=True))
+            env.save_animation(f'{anim_folder}/anim-static-ego.svg', AnimationConfig(egocentric_idx=0, static=True))
+            env.save_animation(f'{anim_folder}/anim-static-no-agents.svg',
+                               AnimationConfig(show_agents=False, static=True))
 
 
 if __name__ == '__main__':
